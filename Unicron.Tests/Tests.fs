f1acbe6f40f7761e263b4e011ad566006aae88d0
Keith LaForce <keith.laforce@levvel.io>
2020-12-12 23:22:33 -0500

    wip

--- Unicron.Tests/Tests.fs
+++ Unicron.Tests/Tests.fs
@@ -3,49 +3,111 @@ module Tests
 open System
 open Xunit
 open Unicron.Checkers
+open Xunit.Abstractions
 
-[<Fact>]
-let ``An empty board position is parsed`` () =
-    let boardState = parseBoardLine "........"
-    let head = boardState |> List.head
-    Assert.Equal(8, boardState |> Seq.length)
-    Assert.Equal(None, head)
+type MyTests(output:ITestOutputHelper) =
 
-[<Fact>]
-let ``A red soldier is parsed`` () =
-    let boardState = parseBoardLine "r......."
-    let head = boardState |> List.head
-    Assert.Equal(Some (Red, Soldier), head)
+    let initialBoard = [
+        ".r.r.r.r";
+        "r.r.r.r.";
+        ".r.r.r.r";
+        "........";
+        "........";
+        "b.b.b.b.";
+        ".b.b.b.b";
+        "b.b.b.b."]
 
-[<Fact>]
-let ``A black soldier is parsed`` () =
-    let boardState = parseBoardLine "b......."
-    let head = boardState |> List.head
-    Assert.Equal(Some (Black, Soldier), head)
+    [<Fact>]
+    let ``An empty board position is parsed`` () =
+        let boardState = parseBoardLine 0 "........"
+        let head = boardState |> List.head
+        Assert.Equal(8, boardState |> Seq.length)
+        Assert.Equal((None,(0,0)), head)
 
-[<Fact>]
-let ``A red king is parsed`` () =
-    let boardState = parseBoardLine "R......."
-    let head = boardState |> List.head
-    Assert.Equal(Some (Red, King), head)
+    [<Fact>]
+    let ``A red soldier is parsed`` () =
+        let boardState = parseBoardLine 0 "r......."
+        let head = boardState |> List.head
+        Assert.Equal((Some (Red, Soldier),(0,0)), head)
 
-[<Fact>]
-let ``A black king is parsed`` () =
-    let boardState = parseBoardLine "B......."
-    let head = boardState |> List.head
-    Assert.Equal(Some (Black, King), head)
+    [<Fact>]
+    let ``A black soldier is parsed`` () =
+        let boardState = parseBoardLine 0 "b......."
+        let head = boardState |> List.head
+        Assert.Equal((Some (Black, Soldier),(0,0)), head)
 
-[<Fact>]
-let ``Invalid state is parsed correctly`` () =
-    let boardState = parseBoardLine "Z......."
-    let head = boardState |> List.head
-    Assert.Equal(None, head)
+    [<Fact>]
+    let ``A red king is parsed`` () =
+        let boardState = parseBoardLine 0 "R......."
+        let head = boardState |> List.head
+        Assert.Equal((Some (Red, King),(0,0)), head)
 
-[<Fact>]
-let ``Full board is parsed correctly`` () =
-    let boardStrings = [ for i in 1 .. 8 -> "........" ]
-    let board = parseBoard boardStrings
-    let head = board |> List.head |> List.head
-    Assert.Equal(None, head)
+    [<Fact>]
+    let ``A black king is parsed`` () =
+        let boardState = parseBoardLine 0 "B......."
+        let head = boardState |> List.head
+        Assert.Equal((Some (Black, King),(0,0)), head)
+
+    [<Fact>]
+    let ``Invalid state is parsed correctly`` () =
+        let boardState = parseBoardLine 0 "Z......."
+        let head = boardState |> List.head
+        Assert.Equal((None,(0,0)), head)
+
+    [<Fact>]
+    let ``Full board is parsed correctly`` () =
+        let boardStrings = [ for i in 1 .. 8 -> "........" ]
+        let board = parseBoard boardStrings
+        let head = board |> List.head |> List.head
+        Assert.Equal((None,(0,0)), head)
+
+    [<Fact>]
+    let ``Is square occupied by red`` () =
+        let boardStrings = [ for i in 1 .. 8 -> "r......." ]
+        let board = parseBoard boardStrings
+        let head = board |> List.head |> List.head
+        let isOccupied = isSquareOccupiedByPlayer head Red
+        Assert.Equal(true, isOccupied)
+
+    [<Fact>]
+    let ``Is square occupied by red king`` () =
+        let boardStrings = [ for i in 1 .. 8 -> "R......." ]
+        let board = parseBoard boardStrings
+        let head = board |> List.head |> List.head
+        let isOccupied = isSquareOccupiedByPlayer head Red
+        Assert.Equal(true, isOccupied)
+
+    [<Fact>]
+    let ``Is square not occupied by red`` () =
+        let boardStrings = [ for i in 1 .. 8 -> "b......." ]
+        let board = parseBoard boardStrings
+        let head = board |> List.head |> List.head
+        let isOccupied = isSquareOccupiedByPlayer head Red
+        Assert.Equal(false, isOccupied)
+
+    [<Fact>]
+    let ``Get red moves should be 0`` () =
+        let board = parseBoard initialBoard
+        let square = board.[0].[1]
+        let moves = getRedMoves board square
+        output.WriteLine("starting location {0}", square)
+        output.WriteLine("available red moves {0}", moves)
+        Assert.Equal(0, moves |> List.length)
+    
+    [<Fact>]
+    let ``Get red moves should be 2`` () =
+        let board = parseBoard initialBoard
+        let square = board.[2].[1]
+        let moves = getRedMoves board square
+        output.WriteLine("starting location {0}", square)
+        output.WriteLine("available red moves {0}", moves)
+        Assert.Equal(2, moves |> List.length)
+
+    [<Fact>]
+    let ``Get legal moves produces two for red`` () =
+        let board = parseBoard initialBoard
+        let head = board |> List.head |> List.head
+        let moves = getLegalNonJumps board Red
+        Assert.Equal(12, moves |> List.length)
 
 
--- Unicron/Library.fs
+++ Unicron/Library.fs
@@ -4,21 +4,101 @@ module Checkers =
     type Player = Red | Black
     type Rank = Soldier | King
     type Piece = Player * Rank
-    type Coord = { Row:int; Column:int}
-    type Board = Piece option list list
+    type Move = ((int * int) * (int * int))
+    type Location = (int * int)
+    type Square = (Piece option * Location)
+    type Board = Square list list
 
-
-    let getBoardState boardStateCharacter : Piece option =
+    let getBoardState row col boardStateCharacter : Square =
         match boardStateCharacter with 
-        | '.' -> None
-        | 'r' -> Some (Red, Soldier)
-        | 'b' -> Some (Black, Soldier)
-        | 'R' -> Some (Red, King)
-        | 'B' -> Some (Black, King)
-        | _ -> None
+        | '.' -> (None, (row, col))
+        | 'r' -> (Some (Red, Soldier), (row, col))
+        | 'b' -> (Some (Black, Soldier), (row, col))
+        | 'R' -> (Some (Red, King), (row, col))
+        | 'B' -> (Some (Black, King), (row, col))
+        | _ -> (None, (row, col))
 
-    let parseBoardLine line =
-        line |> Seq.toList |> List.map getBoardState
+    let parseBoardLine row line =
+        line |> Seq.toList |> List.mapi (getBoardState row)
     
     let parseBoard lines : Board =
-        lines |> List.map parseBoardLine
+        lines |> List.mapi parseBoardLine
+
+    let getLegalJumps board player =
+        []
+
+    let convertLocationToString (location:Location) =
+        let (row, col) = location
+        let colName = match col with 
+                        | 1 -> "A"
+                        | 2 -> "B"
+                        | 3 -> "C"
+                        | 4 -> "D"
+                        | 5 -> "E"
+                        | 6 -> "F"
+                        | 7 -> "G"
+                        | 8 -> "H"
+                        | _ -> ""
+        sprintf "%s%i" colName row
+
+    let isLocationOccupied (board:Board, location:Location) =
+        let (row, col) = location
+        let square = board.[row].[col]
+        match square with
+            | (piece, _) -> match piece with
+                            | None -> false
+                            | Some(_) -> true
+
+    let isSquareOccupiedByPlayer square player =
+        match square with
+        | (piece, _) -> match piece with
+                        | None -> false
+                        | Some (squarePlayer, _) ->
+                            match squarePlayer, player with
+                            | (Red, Red) -> true
+                            | (Black, Black) -> true
+                            | _ -> false
+    
+    let getRedMoves board square =
+        //check diagonal southwest and southeast
+        let _, location = square
+        let (locRow, locCol) = location
+        //on the second row this fails because the col is on the edge
+        let possibleMoves = [(locRow + 1, locCol - 1); (locRow + 1, locCol + 1)]
+        let validLocations = possibleMoves |> List.filter (fun move -> not (isLocationOccupied (board, move)))
+        validLocations |> List.map (fun newLoc -> (location, newLoc))
+
+    let getBlackMoves board square =
+        //check diagonal southwest and southeast
+        let _, location = square
+        let (locRow, locCol) = location
+        let possibleMoves = [(locRow - 1, locCol - 1); (locRow - 1, locCol + 1)]
+        let validLocations = possibleMoves |> List.filter (fun move -> isLocationOccupied (board, move))
+        validLocations |> List.map (fun newLoc -> (location, newLoc))
+
+
+    let hasMove board player square =
+        match player with
+        | Red -> getRedMoves board square
+        | Black -> getBlackMoves board square
+
+    let getLegalNonJumps board player =
+        //process the board and find non jump moves for current player
+        let x = board
+                    |> List.concat
+                    |> List.filter (fun square -> isSquareOccupiedByPlayer square player)
+                    |> List.map (hasMove board player)
+        x
+
+    (*let getLegalMoves (board:Board, player:Player) : Move list =
+        let jumps = getLegalJumps(board,player)
+        match jumps |> List.length with
+        | x when x > 0 -> jumps
+        | x when x = 0 -> getLegalNonJumps(board, player)
+        | _ -> []
+
+    let getNextMove (board:Board, player:Player) : string =
+        //first get a list of legal moves
+        //select the first legal move as a naieve approach
+        let legalMoves =   getLegalMoves(board, player)
+        "E"*)
